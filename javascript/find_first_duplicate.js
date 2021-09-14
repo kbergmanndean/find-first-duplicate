e458f4ed3bce9decd4ecc52629084d59cca261da
Kaia <kbergmanndean@gmail.com>
2021-09-14 16:39:42 -0400

    started solution, doesn/'t work

--- javascript/find_first_duplicate.js
+++ javascript/find_first_duplicate.js
@@ -1,12 +1,13 @@
 function findFirstDuplicate(arr) {
-  const unique=new Set();
-  for (const num of arr) {
-    if (unique.has(num)){
-      return num
-    }
-    unique.add(num);
-  }
-  return -1;
+ //iterate through array
+ for (let i=0;i<arr.length-1;i++){
+   //compare value to value before it
+   if (arr[i]==arr[i-1]){
+     return arr[i]
+   } else {
+      return -1
+   }
+ }
 }
 
 if (require.main === module) {
@@ -32,7 +33,4 @@ module.exports = findFirstDuplicate;
 //if there are no duplicate values return -1. 
 
 //I tried to solve this by iterating through the array with a for loop, using i to represent the index of each value. (for (let i=0;i<arr.length-1;i++){})...
-//Then I tried to see if each value as i increased (arr[i]) was equal to arr[i-1]. This did not work. I learned about Sets, which can only have unique values. 
-//So I created a new Set, then added the numbers from the array to that set, for each number in the array, but first checking if that number was already in the array, and if so, returning that number.
-//If there's no duplicate, exit iteration without returning a value, so return -1
-
+//Then I tried to see if each value as i increased (arr[i]) was equal to arr[i-1]. This did not work.
